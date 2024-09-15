bf849465cd6088ccaa5e9bde9eebc93f3834d524
Tanishka Gupta <130149454+tanishkagupta2207@users.noreply.github.com>
2024-09-15 19:43:47 +0530

    Add files via upload

---
+++ test5/test5.asm
@@ -0,0 +1,22 @@
+ldc 1
+add
+ldc 2
+add
+ldc 3
+add 
+ldc 4
+add 
+ldc 5
+add 
+ldc 6
+		add
+ldc 7
+add
+ldc 8
+   add
+nouse: 
+ldc 9
+add
+last: ldc 10   
+add                        ;addition of 10 no. stored in reg a
+
